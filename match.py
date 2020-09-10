39ca5443b0ba487e2c239b23d30bab451c275e51
Fred Yankowski <fcy@imsa.edu>
2020-09-10 07:49:05 -0500

    finish generalized version

--- match.py
+++ match.py
@@ -14,8 +14,8 @@ logger = logging.getLogger("match")
 
 
 def load_names(f):
-    """ Return list of (name, value) tuples. """
-    return [line.strip() for line in f]
+    """ Read tab-separated data and return its content as list of lists. """
+    return [line.strip().split("\t") for line in f]
 
 
 def match(names, name):
@@ -23,19 +23,21 @@ def match(names, name):
     # print(f"match({name})")
 
     best_score = 0
-    best_name = ""
+    best_match = ""
     for n in names:
-        score = fuzz.ratio(name, n)
-        # logger.debug(f"score of {name} against {n} is {score}")
+        name_test = n[0]
+        score = fuzz.ratio(name, name_test)
+        # logger.debug(f"score of {name} against {name_test} is {score}")
 
         if score > best_score:
             best_score = score
-            best_name = n
+            best_match = n
 
         if score == 100:
+            # Can't get better than this, so save time by stopping the search
             break
 
-    return (best_name, best_score)
+    return (best_match, best_score)
 
 
 def main():
@@ -43,12 +45,13 @@ def main():
     parser.add_argument("matchfile", help="file with values to match")
     parser.add_argument("-d", "--debug", action="store_true", help="log debug messages")
     args = parser.parse_args()
-    logger.debug(f"args = {args}")
 
     if args.debug:
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.WARN)
+
+    logger.debug(f"args = {args}")
 
     with open(args.matchfile) as f:
         names = load_names(f)
@@ -58,10 +61,10 @@ def main():
         name_to_match = line.strip()
         (best, score) = match(names, name_to_match)
         logger.debug(f"{name_to_match} -> {best}  {score}")
-        if score < 90:
+        if score < 80:
             logger.warning(f"low score: {name_to_match} -> {best}  {score}")
 
-        print(f"{name_to_match}\t{best}")
+        print(f"{name_to_match}\t" + "\t".join(best))
 
 
 if __name__ == "__main__":
