211e1541c1b76347e66f5d3ee2580687dc6b40e4
Fred Yankowski <fred@yankowski.com>
2022-10-17 16:22:01 -0500

    add comments; add --minimum

--- match.py
+++ match.py
@@ -1,5 +1,20 @@
-"""
-Generate mapping file needed to upload photos to PowerSchool.
+"""Translate input data using a fuzzy match against reference data.
+
+For each line in the input data we find the best matching line in the reference
+data and emit the combination of those two lines, tab separated.
+
+The input data is considered one entire line at a time (not including the line
+termination characters).
+
+Only the content up to (and not including) the first tab character is considered
+for matching in the reference data.
+
+But the entire line of matching reference data is appended to the input line,
+allowing us to append any additional data columns from the reference data (kind
+of the whole point).
+
+Any cases of suspiciously low fuzzy-match scores are reported to stderr.
+
 """
 
 import argparse
@@ -44,6 +59,9 @@ def main():
     parser = argparse.ArgumentParser(description="Find best fuzzy matches")
     parser.add_argument("matchfile", help="file with values to match")
     parser.add_argument("-d", "--debug", action="store_true", help="log debug messages")
+    default_minimum = 80
+    parser.add_argument("-m", "--minimum", type=int, default=default_minimum,
+                        help=f"minimum fuzzy score (default: {default_minimum})")
     args = parser.parse_args()
 
     if args.debug:
@@ -61,7 +79,7 @@ def main():
         name_to_match = line.strip()
         (best, score) = match(names, name_to_match)
         logger.debug(f"{name_to_match} -> {best}  {score}")
-        if score < 80:
+        if score < args.minimum:
             logger.warning(f"low score: {name_to_match} -> {best}  {score}")
 
         print(f"{name_to_match}\t" + "\t".join(best))
